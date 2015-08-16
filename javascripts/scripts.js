c51a05f2be8803d76fbe6654b37c3129ebbeb004
Skip Teel <lewisteel@gmail.com>
2015-08-16 13:05:19 -0400

    fixes dropdowns for iPhone

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,4 +1,26 @@
 $(document).ready(function(){
+  function isiPhone(){
+      return (
+          //Detect iPhone
+      //var isiPad = navigator.userAgent.match(/iPad/i) != null;
+          (navigator.platform.indexOf("iPhone") != -1) ||
+          //Detect iPod
+          (navigator.platform.indexOf("iPad") != -1)
+      );
+  }
   $('.parallax').parallax();
   $('.slider').slider({full_width: true});
+  if (isiPhone()){
+    $('.btn-flat').hover(function() {
+    	var $parent = jQuery(this);
+    	var $dropdown = $parent.children('ul');
+
+    	$dropdown.show(0,function() {
+    		$parent.mouseleave(function() {
+    			var $this = jQuery(this);
+    			$this.children('ul').fadeOut(10);
+    		});
+    	});
+    });
+  }
 });
